15255c506d678bc5e02d7eb84a6669c79385269e
iBrunao <brunotguima@hotmail.com>
2024-09-14 15:42:02 -0300

    Dia 1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using RestSharp;
+using System;
+using System.Text.Json;
+
+class Program
+{
+    public static void Main()
+    {
+        PokemonList();
+    }
+
+    private static void PokemonList()
+    {
+        var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/");
+        RestRequest request = new RestRequest("", Method.Get);
+        var response = client.Execute(request);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            // Deserializando o conteúdo da resposta em um objeto JSON
+            var options = new JsonSerializerOptions { WriteIndented = true }; // Opção para formatar o JSON com indentação
+            var jsonFormatted = JsonSerializer.Serialize(
+                JsonSerializer.Deserialize<object>(response.Content), options);
+
+            // Imprimindo o JSON formatado
+            Console.WriteLine(jsonFormatted);
+        }
+        else
+        {
+            Console.WriteLine($"Erro: {response.ErrorMessage}");
+        }
+
+        Console.ReadKey();
+    }
+}
